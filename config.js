89952a4d2b0cdd740aea711d18d6ceaaca78ce29
Billy Junaidi <billy.junaidi@gmail.com>
2024-08-01 06:19:21 +0800

    modify config.js for empe

--- config.js
+++ config.js
@@ -19,46 +19,42 @@ console.log(mnemonic)
 console.log("==================================================================")
 
 export default {
-    port: 8088, // http port 
+    port: 8088, // http port
     db: {
-        path: `${HOME}/history.db` // save request states 
+        path: `${HOME}/history.db` // save request states
     },
     project: {
-        name: "Demo of Side Exchange",
-        logo: "https://side.one/favicon.ico",
-        deployer: `<a href="https://demo.side.exchange">Side Exchange</a>`
+        name: "Empeiria Faucet",
+        logo: "https://github.com/CryptoNodeID/explorer/blob/master/public/logos/empe.png?raw=true",
+        deployer: `<a href="https://cryptonode.id">CryptoNodeID</a>`
     },
     blockchains: [
         {
-            name: "rigi-kent",
+            name: "empe-chain",
             endpoint: {
                 // make sure that CORS is enabled in rpc section in config.toml
                 // cors_allowed_origins = ["*"]
-                rpc_endpoint: " https://testnet-rpc.side.one",
+                rpc_endpoint: "https://empe-testnet-rpc.cryptonode.id",
             },
             sender: {
                 mnemonic,
                 option: {
                     hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
-                    prefix: "side" // human readable address prefix
+                    prefix: "empe" // human readable address prefix
                 }
             },
             tx: {
                 amount: [
                     {
-                        denom: "ukent",
-                        amount: "1000000000"
-                    },
-                    {
-                        denom: "uatom",
-                        amount: "10000000000"
+                        denom: "uempe",
+                        amount: "10000000"
                     },
                 ],
                 fee: {
                     amount: [
                         {
-                            amount: "5000",
-                            denom: "ukent"
+                            amount: "1",
+                            denom: "uempe"
                         }
                     ],
                     gas: "200000"
@@ -66,97 +62,11 @@ export default {
             },
             limit: {
                 // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
+                address: 1,
                 // how many times each ip is allowed in a window(24h),
                 // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
+                ip: 2
             }
         },
-        {
-            type: 'Ethermint',
-            ids: {
-                chainId: 1818,
-                cosmosChainId: 'sidechain_1818-1',
-            },
-            name: "Proxima",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: "https://proxima-rpc.side.exchange",
-                evm_endpoint: "http://13.229.237.39:8545/",
-            },
-            sender: {
-                mnemonic,
-                option: {
-                    hdPaths: [stringToPath("m/44'/60")],
-                    prefix: "prox"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "aprox",
-                    amount: "5000000000000000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "100000",
-                            denom: "aprox"
-                        }
-                    ],
-                    gas: "10000000000000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
-        {
-            type: 'Ethermint',
-            ids: {
-                chainId: 1819,
-                cosmosChainId: 'sidechain_1819-1',
-            },
-            name: "Toliman",
-            endpoint: {
-                // make sure that CORS is enabled in rpc section in config.toml
-                // cors_allowed_origins = ["*"]
-                rpc_endpoint: "https://toliman-rpc.side.exchange",
-                evm_endpoint: "http://52.77.209.10:8545/",
-            },
-            sender: {
-                mnemonic,
-                option: {
-                    hdPaths: [stringToPath("m/44'/60/0'/0/0")],
-                    prefix: "toli"
-                }
-            },
-            tx: {
-                amount: {
-                    denom: "atoli",
-                    amount: "5000000000000000000"
-                },
-                fee: {
-                    amount: [
-                        {
-                            amount: "100000",
-                            denom: "atoli"
-                        }
-                    ],
-                    gas: "10000000000000"
-                },
-            },
-            limit: {
-                // how many times each wallet address is allowed in a window(24h)
-                address: 1, 
-                // how many times each ip is allowed in a window(24h),
-                // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10 
-            }
-        },
-    ]    
+    ]
 }
